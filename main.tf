ac7bc6c4df54a1ab53bf9c73d0af2c57eee5e973
MINA <mina.ayoub@avanade.com>
2026-05-15 09:53:44 -0400

    storage account name

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
+resource "azurerm_resource_group" "saccount" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
+
 module "storage_account" {
   source = "./modules/storageaccount"
 
   name                = var.storage_account_name
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.saccount.name
   location            = var.location
   tags                = var.tags
 }
@@ -11,7 +17,7 @@ module "automation_account" {
   source = "./modules/automationaccount"
 
   name                = var.automation_account_name
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.saccount.name
   location            = var.location
   tags                = var.tags
 }
